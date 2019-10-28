6ccaea64b62a7845e96e76443494c3fe77f7b01b
邓俊立 <dengjunli@aipai.com>
2019-10-28 17:00:30 +0800

    commit

---
+++ topic-7.js
@@ -0,0 +1,64 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class ConfirmComp extends React.Component {
+    render() {
+        const { content, onSure, onClose } = this.props;
+        return (
+            <div className='confirm'>
+                <div>{content}</div>
+                <div>
+                    <button
+                        onClick={e => {
+                            onSure(e);
+                        }}
+                    >
+                        确认
+          </button>
+                    <button
+                        onClick={e => {
+                            onClose(e);
+                        }}
+                    >
+                        取消
+          </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+const Confirm = content => {
+    return new Promise((resolve, reject) => {
+        const node = document.createElement("div");
+        document.body.append(node);
+        ReactDOM.render(
+            <ConfirmComp
+                content={content}
+                onSure={e => {
+                    resolve(true);
+                }}
+                onClose={e => {
+                    resolve(false);
+                }}
+            />,
+            node
+        );
+    });
+};
+
+class Demo extends React.Component {
+    render() {
+        return null;
+    }
+    async componentDidMount() {
+        let res = await Confirm("确定删除吗");
+        if (res) {
+            console.log("是");
+        } else {
+            console.log("否");
+        }
+    }
+}
+
+export default Demo;
